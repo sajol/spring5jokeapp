83d2b92e2a91023a2b1a63402878de0c959354a6
sajol <sajolkk@gmail.com>
2017-12-29 19:52:11 +0600

    create bean configuration to use chucknorrisquotes as a spring bean

---
+++ src/main/java/com/spring/jokeapp/config/ChuckNorrisConfiguration.java
@@ -0,0 +1,18 @@
+package com.spring.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author: sazal
+ * Date: 12/29/17
+ */
+@Configuration
+public class ChuckNorrisConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes getChuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
